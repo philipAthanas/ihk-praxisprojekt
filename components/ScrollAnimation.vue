00eda47dc6d2a1b4e058b43713479e33610e0503
Tarik Ermis <t.ermis@integr8.com>
2022-02-17 14:42:20 +0100

    exclude eslint

--- components/ScrollAnimation.vue
+++ components/ScrollAnimation.vue
@@ -3,8 +3,14 @@
     <b-row>
       <b-col>
         <div data-aos="fade-up" data-aos-delay="300">
-          <img src="~/assets/pages/home/hero.svg" >
-          <img data-aos="zoom-in" data-aos-delay="500" src="~/assets/pages/home/hero-2.svg">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSo3RL-xS_5U13YPec8gtjMNnEvNKwZJmmFjfYy6GS6Z1Y-2UmmiYjf9QvI2CyTObh3Plk&usqp=CAU"
+          />
+          <img
+            data-aos="zoom-in"
+            data-aos-delay="500"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9VroFZKcM4u3LrWHS41xQ_TwplPzSxhgnZA&usqp=CAU"
+          />
         </div>
       </b-col>
     </b-row>
@@ -12,9 +18,8 @@
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style scoped>
-
 </style>
