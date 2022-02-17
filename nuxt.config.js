45def3d6ba911d173c6aa419a8cd10a5c4d213a5
Tarik Ermis <t.ermis@integr8.com>
2022-02-17 14:17:48 +0100

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,13 +25,11 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: "/plugins/aos", mode: "client" },
+    { src: "@/plugins/aos", mode: "client" },
   ],
   purgeCSS: {
     whitelist: ["aos-init", "aos-animate", "data-aos-delay", "data-aos-duration", "fade-up", "zoom-in"],
   },
-};
-
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
