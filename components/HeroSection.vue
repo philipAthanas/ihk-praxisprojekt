18603efa3afcf3c059d37d77dbae452ece6746fd
philipAthanas <p.athanas@integr8.com>
2022-05-13 13:41:15 +0200

    Freitag 13. 13h

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,38 +1,75 @@
 <template>
   <div class="header">
-    <div class="box box1">1: titel_text</div>
-    <div class="box box2">2: trennung</div>
-    <div class="box box3">3: team1</div>
-    <div class="box box4">4: team2</div>
-    <div class="box box5">5: logo</div>
+    <div class="box box1">
+      <span class="titel">1: WIR SIND INTEGR8.</span>
+    </div>
 
-    <!--
-    <div class="element item7">7</div>
-    <div class="element item8">8</div>
-    <div class="element item9">9</div>
-    <div class="element item10">10</div>
-    <div class="element item11">11</div>
-    <div class="element item12">12</div>
+    <div class="box box2">
+      <img
+        src="/Users/philip/Documents/Projects/Integr8/i8-plattform/Organigramm/i8-organigramm-2/grid-bilder/Line 11.png"
+        alt="SVG mit Trennlinie"
+      />
+    </div>
 
-    <div class="element item13">13</div>  
-    <div class="element item14">14</div>
-    <div class="element item15">15</div>
-    <div class="element item16">16</div>
-    <div class="element item17">17</div>  
-    <div class="element item18">18</div>
-    <div class="element item19">19</div>
-    <div class="element item20">20</div>
-    <div class="element item21">21</div>
-    <div class="element item22">22</div>
-    <div class="element item23">23</div>
-    <div class="element item24">24</div>
--->
+    <div class="box box3">
+      <div id="square-border">
+        <p>3: Developement</p>
+        <div id="circle"></div>
+      </div>
+    </div>
+
+    <div class="box box4">
+      4: operation
+      <div id="team">
+        <div id="rechteck"></div>
+        <div id="kreis"></div>
+        <div id="text">Operation</div>
+      </div>
+    </div>
+    <div class="box box5">
+      <img
+        src="grid-bilder/i8-logo.png"
+        class="img-fluid"
+        alt="INTEGER8 Logo"
+      />
+    </div>
+    <div class="box box6">
+      <span class="i8-text"
+        ><p>
+          6-11: Als Architekt für den digitalen Raum planen, organisieren und
+          lenken wir außergewöhnliche Projekte. Wir entwickeln individuelle
+          Lösungen, die Technologie, Kreation und Aktivierung zielführend
+          vereinen. Um unser Ziel zu erreichen, bieten wir außergewöhnlichen
+          Talenten eine Umgebung, in der sie neues lernen wollen und inspiriert
+          arbeiten können.
+        </p>
+      </span>
+    </div>
+    <div class="box box7">
+      7
+      <div class="d-grid gap-2">
+        <button class="btn btn-primary" type="button">Stategy</button>
+      </div>
+    </div>
+    <div class="box box8">8: strategy</div>
+    <div class="box box9">9: marketing</div>
+    <div class="box box10">
+      10
+      <div class="d-grid gap-2">
+        <button class="btn btn-primary" type="button">Creative</button>
+      </div>
+    </div>
+    <div class="box box11">11</div>
+    <div class="box box12">12</div>
+    <div class="box box13">13: creative</div>
+    <div class="box box14">14</div>
+    <div class="box box15">15</div>
   </div>
 </template>
 
 <script>
 export default {
-  css: ["~/assets/css/main3.css"],
+  css: ["~/assets/css/style.css"],
 };
 </script>
 
--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,7 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    '@/assets/css/main3.css'
+    '@/assets/css/style.css'
 ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
