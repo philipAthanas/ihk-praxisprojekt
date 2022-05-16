8f63ff53db90f0d0daea1a5768703abc5055e324
philipAthanas <p.athanas@integr8.com>
2022-05-16 15:56:01 +0200

    Mo-16-5-final

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,46 +1,7 @@
 <template>
   <div class="header">
     <div class="box box1">
-      <span class="titel">1: WIR SIND INTEGR8.</span>
-    </div>
-
-    <div class="box box2">
-      2
-      <img
-        class="trenlinie"
-        :src="require('../grid-bilder/Line 11.png')"
-        fluid
-        alt="png mit Trennlinie"
-      />
-    </div>
-
-    <div class="box box3">
-      3
-      <div id="square-border">
-        <p>3: Developement</p>
-        <div id="circle"></div>
-      </div>
-    </div>
-
-    <div class="box box4">
-      4: operation
-      <div id="team">
-        <div id="rechteck"></div>
-        <div id="kreis"></div>
-        <div id="text">Operation</div>
-      </div>
-    </div>
-
-    <div class="box box5">
-      5
-      <img
-        class="i8-logo"
-        :src="require('../grid-bilder/i8-logo.png')"
-        fluid
-        alt="INTEGER8 Logo"
-      />
-    </div>
-    <div class="box box6">
+      <span class="titel">WIR SIND INTEGR8.</span>
       <span class="i8-text"
         ><p>
           6-11: Als Architekt für den digitalen Raum planen, organisieren und
@@ -52,25 +13,41 @@
         </p>
       </span>
     </div>
+
+    <div class="box box2">
+      <img
+        class="trenlinie"
+        :src="require('../grid-bilder/Line 11.png')"
+        fluid
+        alt="png mit Trennlinie"
+      />
+    </div>
+
+    <div class="box box3">
+      <TeamBox :teamName="'Developement'" :colorCircle="'green'" />
+    </div>
+
+    <div class="box box4">
+      <TeamBox :teamName="'Operation'" :colorCircle="'red'" />
+    </div>
+    <div class="box box5">
+      <TeamBox :teamName="'Creative'" :colorCircle="'orange'" />
+    </div>
+    <div class="box box6">
+      <TeamBox :teamName="'Strategy'" :colorCircle="'purple'" />
+    </div>
     <div class="box box7">
-      7
-      <div class="d-grid gap-2">
-        <button class="btn btn-primary" type="button">Stategy</button>
-      </div>
+      <TeamBox :teamName="'Marketing'" :colorCircle="'blue'" />
     </div>
-    <div class="box box8">8: strategy</div>
-    <div class="box box9">9: marketing</div>
-    <div class="box box10">
-      10
-      <div class="d-grid gap-2">
-        <button class="btn btn-primary" type="button">Creative</button>
-      </div>
+
+    <div class="box box8">
+      <img
+        class="i8-logo"
+        :src="require('../grid-bilder/i8-logo.png')"
+        fluid
+        alt="INTEGER8 Logo"
+      />
     </div>
-    <div class="box box11">11</div>
-    <div class="box box12">12</div>
-    <div class="box box13">13: creative</div>
-    <div class="box box14">14</div>
-    <div class="box box15">15</div>
   </div>
 </template>
 
---
+++ components/TeamBox.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="square-border">
+    <div class="circle" :style="{ backgroundColor: colorCircle }"></div>
+      <h4>{{ teamName }}</h4>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["teamName", "colorCircle"],
+};
+</script>
+
+<style scoped>
+.circle {
+
+  
+  width: 20px;
+  height: 20px;
+  border-radius: 100%;
+}
+
+.square-border {
+  
+  display: flex;
+  background: #FFF;
+  border: 1px solid #000000;
+  padding: 22px;
+  text-align: center;
+  align-items: center;
+  
+}
+
+h4 {
+  margin: 0px;
+  margin-left: 15px;
+}
+</style>
